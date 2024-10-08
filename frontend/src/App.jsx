e8359583361c022cea607f5bc09c18526f5ad971
Om Tewari <omtewari106@gmail.com>
2024-10-08 10:15:25 +0530

    integrate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import SignUp from './components/Signup';
-import Login from './components/Login';
+import SignUp from './pages/Signup';
+import Login from './pages/Login';
+import Home from './components/Home';
+import Courses from './pages/Courses';
+import Blog from './pages/Blog';
 
 function App() {
   return (
@@ -13,8 +16,12 @@ function App() {
           <Link className="text-blue-500 hover:underline" to="/login">Login</Link>
         </nav>
         <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+
         </Routes>
       </div>
     </Router>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 // src/pages/Home.js
 import React from 'react';
-import Header from '../components/Header';
-import Categories from '../components/Categories';
+import Header from './Header';
+import Categories from './Categories';
 
 const Home = () => {
   return (
